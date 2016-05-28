db0097c8443f8ab0ba3eabc51b9e7165dd26a191
Gabe Appleton <gappleto97+development@gmail.com>
2016-05-27 22:34:20 -0700

    Correct errors in construct_message

--- js/p2p.js
+++ js/p2p.js
@@ -2,7 +2,13 @@ var BigInt = require('./BigInteger/BigInteger.js')
 var struct = require('./pack/jspack.js')
 var SHA = require('./SHA/src/sha.js')
 
-version = "0.1.C"
+var version = "0.1.C"
+
+// User salt generation pulled from: http://stackoverflow.com/a/2117523
+var user_salt = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+    return v.toString(16);
+});
 
 var to_base_58 = function(i) {
     //Takes an integer and returns its corresponding base_58 string
@@ -48,6 +54,7 @@ var SHA256 = function(text) {
     return hash.getHash("HEX");
 }
 
+
 class protocol {
     constructor(sep, subnet, encryption) {
         this.sep = sep
@@ -61,6 +68,7 @@ class protocol {
     }
 }
 
+
 var unpack = function(fmt, str) {
     arr = [];
     for(var i=0; i<str.length; i++) {
@@ -69,10 +77,12 @@ var unpack = function(fmt, str) {
     return struct.Unpack(fmt, arr)
 }
 
+
 var pack = function(fmt, vals) {
     res = struct.Pack(fmt, vals)
     return String.fromCharCode.apply(null, res)
 }
+
 
 var construct_message = function(prot, comp_types, msg_type, id, packets, time) {
     var time = typeof time !== 'undefined' ?  time : to_base_58(getUTC());
@@ -80,8 +90,8 @@ var construct_message = function(prot, comp_types, msg_type, id, packets, time) 
     var msg_hash = SHA384(packets.join(prot.sep) + time)
     var msg_id = to_base_58(BigInt(msg_hash, 16))
 
-    packets = [msg_type, id, msg_id, time].concat(packets)
-    msg = packets.join(prot.sep)
+    var packets = [msg_type, id, msg_id, time].concat(packets)
+    var msg = packets.join(prot.sep)
 
     //compression_used = ""
     //for method in compression:
@@ -90,6 +100,6 @@ var construct_message = function(prot, comp_types, msg_type, id, packets, time) 
     //        msg = compress(msg, method)
     //        break
 
-    size = pack("!L", msg.length)
-    return size, msg
+    var size = pack("!L", [msg.length])
+    return [size, msg]
 }
