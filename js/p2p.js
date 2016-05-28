1d709eb42f86f29f1ea38c2d9c67e6438b88625d
Gabe Appleton <gappleto97+development@gmail.com>
2016-05-27 23:31:56 -0700

    Add bufferpack to replace jspack

--- js/p2p.js
+++ js/p2p.js
@@ -1,8 +1,8 @@
-var BigInt = require('./BigInteger/BigInteger.js')
-var struct = require('./pack/jspack.js')
-var SHA = require('./SHA/src/sha.js')
+var BigInt = require('./BigInteger/BigInteger.js');
+var struct = require('./pack/bufferpack.js');
+var SHA = require('./SHA/src/sha.js');
 
-var version = "0.1.C"
+var version = "0.1.C";
 
 // User salt generation pulled from: http://stackoverflow.com/a/2117523
 var user_salt = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
@@ -12,32 +12,31 @@ var user_salt = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function
 
 var to_base_58 = function(i) {
     //Takes an integer and returns its corresponding base_58 string
-    string = ""
+    string = "";
     if (!BigInt.isInstance(i)) {
-        i = BigInt(i)
+        i = BigInt(i);
     }
     while (i.notEquals(0)) {
-        string = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'[i.mod(58)] + string
-        i = i.divide(58)
+        string = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'[i.mod(58)] + string;
+        i = i.divide(58);
     }
-    return string
+    return string;
 }
 
 
 var from_base_58 = function(string) {
     //Takes a base_58 string and returns its corresponding integer
-    decimal = BigInt(0)
+    decimal = BigInt(0);
     //for char in string {
     for (i = 0; i < string.length; i++) {
-        console.log(decimal)
-        decimal = decimal.times(58).plus('123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'.indexOf(string[i]))
+        decimal = decimal.times(58).plus('123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'.indexOf(string[i]));
     }
-    return decimal
+    return decimal;
 }
 
 
 var getUTC = function() {
-    return Math.floor(Date.now() / 1000)
+    return Math.floor(Date.now() / 1000);
 }
 
 
@@ -57,41 +56,26 @@ var SHA256 = function(text) {
 
 class protocol {
     constructor(sep, subnet, encryption) {
-        this.sep = sep
-        this.subnet = subnet
-        this.encryption = encryption
+        this.sep = sep;
+        this.subnet = subnet;
+        this.encryption = encryption;
     }
 
     id() {
-        var protocol_hash = SHA256([this.sep, this.subnet, this.encryption, version].join(''))
-        return to_base_58(BigInt(protocol_hash, 16))
+        var protocol_hash = SHA256([this.sep, this.subnet, this.encryption, version].join(''));
+        return to_base_58(BigInt(protocol_hash, 16));
     }
-}
-
-
-var unpack = function(fmt, str) {
-    arr = [];
-    for(var i=0; i<str.length; i++) {
-        arr.push(str.charCodeAt(i))
-    }
-    return struct.Unpack(fmt, arr)
-}
-
-
-var pack = function(fmt, vals) {
-    res = struct.Pack(fmt, vals)
-    return String.fromCharCode.apply(null, res)
 }
 
 
 var construct_message = function(prot, comp_types, msg_type, id, packets, time) {
     var time = typeof time !== 'undefined' ?  time : to_base_58(getUTC());
 
-    var msg_hash = SHA384(packets.join(prot.sep) + time)
-    var msg_id = to_base_58(BigInt(msg_hash, 16))
+    var msg_hash = SHA384(packets.join(prot.sep) + time);
+    var msg_id = to_base_58(BigInt(msg_hash, 16));
 
-    var packets = [msg_type, id, msg_id, time].concat(packets)
-    var msg = packets.join(prot.sep)
+    var packets = [msg_type, id, msg_id, time].concat(packets);
+    var msg = new Buffer(packets.join(prot.sep));
 
     //compression_used = ""
     //for method in compression:
@@ -100,6 +84,6 @@ var construct_message = function(prot, comp_types, msg_type, id, packets, time) 
     //        msg = compress(msg, method)
     //        break
 
-    var size = pack("!L", [msg.length])
-    return [size, msg]
+    var size = struct.pack("!L", [msg.length]);
+    return [size, msg];
 }
