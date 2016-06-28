b8f30a08fa149e2ccaa309fd1e0caf34b06fa2b8
Gabe Appleton <gappleto97+development@gmail.com>
2016-06-27 22:55:54 -0700

    Add test_protocol_rejection

--- py_src/test/test_mesh.py
+++ py_src/test/test_mesh.py
@@ -28,3 +28,17 @@ def test_propagation_Plaintext(iters=3):
 
 def test_propagation_SSL(iters=3):
     propagation_validation(iters, 6670, 3, 'SSL')
+
+def protocol_rejection_validation(iters, start_port, num_nodes, encryption):
+    for i in xrange(iters):
+        f = mesh.mesh_socket('localhost', start_port + i*2, prot=mesh.protocol('test', encryption))
+        g = mesh.mesh_socket('localhost', start_port + i*2 + 1, prot=mesh.protocol('test2', encryption))
+        f.connect('localhost', start_port + i*2)
+        time.sleep(0.5)
+        assert len(f.routing_table) == len(f.awaiting_ids) == len(g.routing_table) == len(g.awaiting_ids) == 0
+
+def test_protocol_rejection_Plaintext(iters=3):
+    protocol_rejection_validation(iters, 5000, 3, 'Plaintext')
+
+def test_protocol_rejection_SSL(iters=3):
+    protocol_rejection_validation(iters, 6700, 3, 'SSL')
