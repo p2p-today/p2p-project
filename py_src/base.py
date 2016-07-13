cb07244a82db7e8b5f8af8b8a3f003a0b8f831c1
Gabe Appleton <gappleto97+development@gmail.com>
2016-07-12 17:49:27 -0700

    Remove unicode_literal

--- py_src/base.py
+++ py_src/base.py
@@ -1,7 +1,6 @@
 """A library to store common functions and protocol definitions"""
 
 from __future__ import print_function
-from __future__ import unicode_literals
 from __future__ import absolute_import
 
 import hashlib
