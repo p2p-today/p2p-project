88af7ff225e2c9e894d23f9db41fd2b17b082dcc
Gabe Appleton <gappleto97+development@gmail.com>
2016-07-16 14:54:27 -0700

    Bring setup in line with extra requirements

--- py_src/ssl_wrapper.py
+++ py_src/ssl_wrapper.py
@@ -1,85 +1,67 @@
 from __future__ import with_statement
 
+import datetime
 import os
 import socket
 import ssl
 import sys
 import tempfile
+import uuid
 
-try:
-    from OpenSSL import crypto
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.serialization import Encoding, PrivateFormat, NoEncryption
+from cryptography.x509.oid import NameOID
 
-    def generate_self_signed_cert(cert_file, key_file):
-        """Given two file-like objects, generate an SSL key and certificate."""
-        # create a key pair
-        key = crypto.PKey()
-        key.generate_key(crypto.TYPE_RSA, 2048)
 
-        # create a self-signed cert
-        cert = crypto.X509()
-        cert.get_subject().C = 'PY'
-        cert.get_subject().ST = 'py2p generated cert'
-        cert.get_subject().L = 'py2p generated cert'
-        cert.get_subject().O = 'py2p generated cert'
-        cert.get_subject().OU = 'py2p generated cert'
-        cert.get_subject().CN = socket.gethostname()
-        cert.set_serial_number(1000)
-        cert.gmtime_adj_notBefore(0)
-        cert.gmtime_adj_notAfter(10 * 365 * 24 * 60 * 60) 
-        cert.set_issuer(cert.get_subject())
-        cert.set_pubkey(key)
-        cert.sign(key, 'sha1')
+if sys.version_info < (3, ):
+    import atexit
+    cleanup_files = []
 
-        cert_file.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
-        key_file.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key))
+    def cleanup():  # pragma: no cover
+        for f in cleanup_files:
+            os.remove(f)
 
-except ImportError:
-    try:
-        from cryptography import x509
-        from cryptography.hazmat.backends import default_backend
-        from cryptography.hazmat.primitives import hashes
-        from cryptography.hazmat.primitives.asymmetric import rsa
-        from cryptography.hazmat.primitives.serialization import Encoding, PrivateFormat, NoEncryption
-        from cryptography.x509.oid import NameOID
-        import datetime, uuid
+    atexit.register(cleanup)
 
-        def generate_self_signed_cert(cert_file, key_file):
-            """Given two file-like objects, generate an SSL key and certificate."""
-            one_day = datetime.timedelta(1, 0, 0)
-            private_key = rsa.generate_private_key(
-                public_exponent=65537,
-                key_size=2048,
-                backend=default_backend()
-            )
-            public_key = private_key.public_key()
-            builder = x509.CertificateBuilder()
-            builder = builder.subject_name(x509.Name([
-                x509.NameAttribute(NameOID.COMMON_NAME, u'cryptography.io'),
-            ]))
-            builder = builder.issuer_name(x509.Name([
-                x509.NameAttribute(NameOID.COMMON_NAME, u'cryptography.io'),
-            ]))
-            builder = builder.not_valid_before(datetime.datetime.today() - one_day)
-            builder = builder.not_valid_after(datetime.datetime.today() + datetime.timedelta(365*10))
-            builder = builder.serial_number(int(uuid.uuid4()))
-            builder = builder.public_key(public_key)
-            builder = builder.add_extension(
-                x509.BasicConstraints(ca=False, path_length=None), critical=True,
-            )
-            certificate = builder.sign(
-                private_key=private_key, algorithm=hashes.SHA256(),
-                backend=default_backend()
-            )
 
-            key_file.write(private_key.private_bytes(
-                Encoding.PEM, 
-                PrivateFormat.TraditionalOpenSSL,
-                NoEncryption()
-            ))
-            cert_file.write(certificate.public_bytes(Encoding.PEM))
+def generate_self_signed_cert(cert_file, key_file):
+    """Given two file-like objects, generate an SSL key and certificate."""
+    one_day = datetime.timedelta(1, 0, 0)
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+    public_key = private_key.public_key()
+    builder = x509.CertificateBuilder()
+    builder = builder.subject_name(x509.Name([
+        x509.NameAttribute(NameOID.COMMON_NAME, u'cryptography.io'),
+    ]))
+    builder = builder.issuer_name(x509.Name([
+        x509.NameAttribute(NameOID.COMMON_NAME, u'cryptography.io'),
+    ]))
+    builder = builder.not_valid_before(datetime.datetime.today() - one_day)
+    builder = builder.not_valid_after(datetime.datetime.today() + datetime.timedelta(365*10))
+    builder = builder.serial_number(int(uuid.uuid4()))
+    builder = builder.public_key(public_key)
+    builder = builder.add_extension(
+        x509.BasicConstraints(ca=False, path_length=None), critical=True,
+    )
+    certificate = builder.sign(
+        private_key=private_key, algorithm=hashes.SHA256(),
+        backend=default_backend()
+    )
 
-    except ImportError:  # pragma: no cover
-        raise
+    key_file.write(private_key.private_bytes(
+        Encoding.PEM, 
+        PrivateFormat.TraditionalOpenSSL,
+        NoEncryption()
+    ))
+    cert_file.write(certificate.public_bytes(Encoding.PEM))
+
 
 def get_socket(server_side):
     if server_side:
@@ -97,13 +79,3 @@ def get_socket(server_side):
         return sock
     else:
         return ssl.wrap_socket(socket.socket(), server_side=False, suppress_ragged_eofs=True)
-
-if sys.version_info < (3, ):
-    import atexit
-    cleanup_files = []
-
-    def cleanup():  # pragma: no cover
-        for f in cleanup_files:
-            os.remove(f)
-
-    atexit.register(cleanup)
--- setup.py
+++ setup.py
@@ -1,28 +1,70 @@
-# from distutils.core import setup
-from setuptools import setup
+from __future__ import with_statement
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 from py_src import __version__
 
-setup(name='py2p',
-      description='A python library for peer-to-peer networking',
-      version=__version__,
-      author='Gabe Appleton',
-      author_email='gappleto97+development@gmail.com',
-      url='https://github.com/gappleto97/p2p-project',
-      license='LGPLv3',
-      packages=['py2p', 'py2p.test'],
-      package_dir={'py2p': 'py_src'},
-      classifiers=['Development Status :: 3 - Alpha',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-                   'Programming Language :: Python :: 2',
-                   'Programming Language :: Python :: 2.6',
-                   'Programming Language :: Python :: 2.7',
-                   'Programming Language :: Python :: 3',
-                   'Programming Language :: Python :: 3.3',
-                   'Programming Language :: Python :: 3.4',
-                   'Programming Language :: Python :: 3.5',
-                   'Programming Language :: Python',
-                   'Programming Language :: JavaScript',
-                   'Operating System :: OS Independent',
-                   'Topic :: Communications',
-                   'Topic :: Internet'])
+classifiers = ['Development Status :: 3 - Alpha',
+               'Intended Audience :: Developers',
+               'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+               'Operating System :: OS Independent',
+               'Topic :: Communications',
+               'Topic :: Internet',
+               'Programming Language :: JavaScript',
+               'Programming Language :: Python']
+
+classifiers.extend([
+               ('Programming Language :: Python :: %s' % x) for x in
+                '2 3 2.6 2.7 3.3 3.4 3.5'.split()])
+
+with open(os.path.join('py_src', 'README.rst'), 'r') as fd:
+    long_description = fd.read()
+
+def has_environment_marker_support():
+    """
+    Tests that setuptools has support for PEP-426 environment marker support.
+    The first known release to support it is 0.7 (and the earliest on PyPI seems to be 0.7.2
+    so we're using that), see: http://pythonhosted.org/setuptools/history.html#id142
+    References:
+    * https://wheel.readthedocs.io/en/latest/index.html#defining-conditional-dependencies
+    * https://www.python.org/dev/peps/pep-0426/#environment-markers
+    Method extended from pytest. Credit goes to developers there.
+    """
+    try:
+        from pkg_resources import parse_version
+        return parse_version(setuptools.__version__) >= parse_version('0.7.2')
+    except Exception as exc:
+        sys.stderr.write("Could not test setuptool's version: %s\n" % exc)
+        return False
+
+def main():
+    install_requires = []
+    extras_require = {'SSL': ['cryptography']}
+    if has_environment_marker_support():
+        pass
+    else:
+        pass
+
+    setup(name='py2p',
+          description='A python library for peer-to-peer networking',
+          long_description=long_description,
+          version=__version__,
+          author='Gabe Appleton',
+          author_email='gappleto97+development@gmail.com',
+          url='https://github.com/gappleto97/p2p-project',
+          license='LGPLv3',
+          packages=['py2p', 'py2p.test'],
+          package_dir={'py2p': 'py_src'},
+          classifiers=classifiers,
+          install_requires=install_requires,
+          extras_require=extras_require
+    )
+
+if __name__ == "__main__":
+    main()
