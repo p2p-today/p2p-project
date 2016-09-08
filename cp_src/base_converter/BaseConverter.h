ab396559061f1d994a91a411a99440128257abdb
Gabe Appleton <gappleto97+development@gmail.com>
2016-09-08 17:04:59 -0400

    Correct some comments and remove debug prints

--- cp_src/base_converter/BaseConverter.h
+++ cp_src/base_converter/BaseConverter.h
@@ -17,7 +17,7 @@ extern "C" {
 const static char *base_58 = (char *)"123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
 const static char *ascii   = (char *)"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3a\x3b\x3c\x3d\x3e\x3f\x40\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4a\x4b\x4c\x4d\x4e\x4f\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5a\x5b\x5c\x5d\x5e\x5f\x60\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7a\x7b\x7c\x7d\x7e\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff";
 
-static inline size_t find_base_58(char search)  {
+static inline size_t find_base_58(const char search)  {
     for (size_t i = 0; i < 58; i++) {
         if (base_58[i] == search)
             return i;
@@ -25,7 +25,7 @@ static inline size_t find_base_58(char search)  {
     return -1;
 }
 
-static inline unsigned long long from_base_58(const char *str, size_t len) {
+static inline unsigned long long from_base_58(const char *str, const size_t len) {
     unsigned long long ret = 0;
     for (unsigned int i = 0; i < len; i++)    {
         ret *= (unsigned long long) 58;
@@ -93,9 +93,8 @@ static unsigned int divide_58(char *x, size_t *length)  {
 
         const unsigned int value = base2dec(x, j);
 
-        quotient[pos] = (unsigned char)(value / 58);
-        if (pos != 0 || quotient[pos] != ascii[0])
-            pos++;
+        if (pos != 0 || quotient[pos] != ascii[0])  // prevent leading zeros
+            quotient[pos++] = (unsigned char)(value / 58);
 
         dec2base(value % 58, dec2base_str, &len);
         memmove(x + len, x + j, (*length) - j);
@@ -108,7 +107,7 @@ static unsigned int divide_58(char *x, size_t *length)  {
     // calculate remainder
     const unsigned int remainder = base2dec(x, *length);
 
-    // remove leading "zeros" from quotient and store in 'x'
+    // store quotient in 'x'
     memcpy(x, quotient, pos);
     free(quotient);
     *length = pos;
@@ -126,7 +125,6 @@ static char *ascii_to_base_58_(const char *input, size_t length, size_t *res_len
 
     do  {
         result[--pos] = base_58[divide_58(c_input, &length)];
-        printf("%i\n", result[pos]);
     }
     while (length && !(length == 1 && c_input[0] == ascii[0]));
 
@@ -134,8 +132,6 @@ static char *ascii_to_base_58_(const char *input, size_t length, size_t *res_len
 
     *res_len = res_size - pos;
     memmove(result, result + pos, *res_len);
-    fwrite(result, *res_len, 1, stdout);
-    printf("\n");
     return result;
 }
 
