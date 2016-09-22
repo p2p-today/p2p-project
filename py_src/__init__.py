d36a3ad5b0e027014a6edf6a1c3a9d491cb1bf21
Gabe Appleton <gappleto97+development@gmail.com>
2016-09-22 00:12:41 -0400

    pep8

--- py_src/__init__.py
+++ py_src/__init__.py
@@ -4,24 +4,31 @@ In this module:
 
     Constants
 
-      * __version__: A string containing the major, minor, and patch release number.
+      * __version__: A string containing the major, minor, and patch release
+                        number.
       * version_info: A tuple version of the above
-      * protocol_version: A string containing the major and minor release number. This refers to the underlying protocol
-      * node_policy_version: A string containing the build number associated with this version. This refers to the node and its policies.
+      * protocol_version: A string containing the major and minor release number.
+                            This refers to the underlying protocol
+      * node_policy_version: A string containing the build number associated
+                                with this version. This refers to the node
+                                and its policies.
 
     Classes
 
-      * mesh_socket(addr, port, out_addr=None, prot=py2p.mesh.default_protocol, debug_level=0): 
+      * mesh_socket(addr, port, out_addr=None, debug_level=0,
+                    prot=py2p.mesh.default_protocol):
           -  addr: The address you'd like to bind to
           -  port: The port you'd like to bind to
-          -  out_addr: Your outward-facing address, if that is different from (addr, port)
+          -  out_addr: Your outward-facing address, if that is different
+                        from (addr, port)
           -  prot: The py2p.base.protocol object you'd like to use
           -  debug_level: The verbosity at which this and its associated
              py2p.mesh.mesh_daemon prints debug information
 
 Submodules:
 
-    * base:        A library of common functions and classes to enable mesh and the planned chord
+    * base:        A library of common functions and classes to enable mesh
+                    and the planned chord
     * mesh:        A library to deal with mesh networking
     * chord:       A planned library to deal with distributed hash tables
     * ssl_wrapper: A shortcut library to generate peer-to-peer ssl.SSLSockets
@@ -37,6 +44,7 @@ from .mesh import mesh_socket
 
 __version__ = version
 version_info = tuple(map(int, __version__.split(".")))
+
 
 def bootstrap(socket_type, proto, addr, port, *args, **kargs):
     raise NotImplementedError
