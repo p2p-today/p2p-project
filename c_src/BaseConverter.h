e072768abce35f8beb67e6f1c6e7c380a5b115a4
Gabe Appleton <gappleto97+development@gmail.com>
2016-10-08 13:05:33 -0400

    Document base conversion module

--- c_src/BaseConverter.h
+++ c_src/BaseConverter.h
@@ -3,7 +3,7 @@
 * ==============
 *
 * Arbitrary precision base conversion by Daniel Gehriger <gehriger@linkcad.com>
-* Permission for use was given `here <http://archive.is/BFA8H#17%>`_.
+* Permission for use was given `here <https://archive.is/BFA8H#17%>`_.
 * This has been heavily modified since copying, has been hardcoded for a specific case, then translated to C.
 */
 
@@ -19,8 +19,31 @@ using namespace std;
 extern "C" {
 #endif
 
+/**
+* C/C++ Section
+* ~~~~~~~~~~~~~
+*
+* .. c:var:: const static char *base_58
+*
+*     This buffer contains all of the characters within the base_58 "alphabet"
+*
+* .. c:var:: const static char *ascii
+*
+*     This buffer contains all of the characters within the extended ascii "alphabet"
+*/
+
 const static char *base_58 = (char *)"123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
 const static char *ascii   = (char *)"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3a\x3b\x3c\x3d\x3e\x3f\x40\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4a\x4b\x4c\x4d\x4e\x4f\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5a\x5b\x5c\x5d\x5e\x5f\x60\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7a\x7b\x7c\x7d\x7e\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff";
+
+/**
+* .. c:function:: static inline size_t find_base_58(const char search)
+*
+*     This is the equivalent of base_58.indexOf(search)
+*
+*     :param search: The character you would like to search for
+*
+*     :returns: The index of this character in base_58, or -1
+*/
 
 static inline size_t find_base_58(const char search)  {
     for (size_t i = 0; i < 58; i++) {
@@ -29,6 +52,17 @@ static inline size_t find_base_58(const char search)  {
     }
     return -1;
 }
+
+/**
+* .. c:function:: static inline unsigned long long from_base_58(const char *str, const size_t len)
+*
+*     This converts a short base_58 buffer to its ascii equivalent
+*
+*     :param str: The buffer you wish to convert
+*     :param len: The length of the buffer to convert
+*
+*     :returns: The equivalent integral value
+*/
 
 static inline unsigned long long from_base_58(const char *str, const size_t len) {
     unsigned long long ret = 0;
@@ -39,6 +73,17 @@ static inline unsigned long long from_base_58(const char *str, const size_t len)
     return ret;
 }
 
+/**
+* .. c:function:: static inline unsigned int base2dec(const char *value, const size_t len)
+*
+*     Converts a small ascii buffer to its equivalent integral value
+*
+*     :param value: The buffer you wish to convert
+*     :param len:   The length of the buffer to convert
+*
+*     :returns: The equivalent integral value
+*/
+
 static inline unsigned int base2dec(const char *value, const size_t len)  {
     unsigned int result = 0;
     for (size_t i = 0; i < len; ++i) {
@@ -48,6 +93,20 @@ static inline unsigned int base2dec(const char *value, const size_t len)  {
 
     return result;
 }
+
+/**
+* .. c:function:: static inline void dec2base(unsigned int value, char *result, size_t *len)
+*
+*     Converts an integral value to its equivalent binary buffer, then places this in result and updates len
+*
+*     :param value:     The value you wish to convert (as an unsigned int)
+*     :param result:    The buffer result
+*     :param len:       The length of the buffer result
+*
+*     .. note::
+*
+*         This uses :c:func:`memmove` to transfer data, so it's helpful if you start with a larger-than-necessary buffer
+*/
 
 static inline void dec2base(unsigned int value, char *result, size_t *len)  {
     size_t pos = 4;
@@ -60,6 +119,21 @@ static inline void dec2base(unsigned int value, char *result, size_t *len)  {
     *len = 4 - pos;
     memmove(result, result + pos, *len);
 }
+
+/**
+* .. c:function:: static char *to_base_58(unsigned long long i, size_t *len)
+*
+*     Converts an integral value to base_58, then updates len
+*
+*     :param i:     The value you want to convert
+*     :param len:   The length of the generated buffer
+*
+*     :returns: A buffer containing the base_58 equivalent of ``i``
+*
+*     .. note::
+*
+*         The return value needs to have :c:func:`free` called on it at some point
+*/
 
 static char *to_base_58(unsigned long long i, size_t *len) {
     size_t pos = 0;
@@ -83,6 +157,17 @@ static char *to_base_58(unsigned long long i, size_t *len) {
     *len = pos;
     return str;
 }
+
+/**
+* .. c:function:: static unsigned int divide_58(char *x, size_t *length)
+*
+*     Divides an ascii buffer by 58, and returns the remainder
+*
+*     :param x:         The binary buffer you wish to divide
+*     :param length:    The length of the buffer
+*
+*     :returns: An unsigned int which contains the remainder of this division
+*/
 
 static unsigned int divide_58(char *x, size_t *length)  {
     const size_t const_length = *length;
@@ -121,6 +206,18 @@ static unsigned int divide_58(char *x, size_t *length)  {
     return remainder;
 }
 
+/**
+* .. c:function:: static char *ascii_to_base_58_(const char *input, size_t length, size_t *res_len)
+*
+*     Converts an arbitrary ascii buffer to its base_58 equivalent. The length of this buffer is placed in res_len.
+*
+*     :param input:     An input buffer
+*     :param length:    The length of said buffer
+*     :param res_len:   A pointer to the return buffer's length
+*
+*     :returns: A buffer containing the base_58 equivalent of the provided buffer.
+*/
+
 static char *ascii_to_base_58_(const char *input, size_t length, size_t *res_len)    {
     char *c_input = (char*)malloc(sizeof(char) * length);
     memcpy(c_input, input, length);
@@ -141,6 +238,20 @@ static char *ascii_to_base_58_(const char *input, size_t length, size_t *res_len
     return result;
 }
 
+/**
+* .. c:function:: static char *ascii_to_base_58(const char *input, size_t length, size_t *res_len, size_t minDigits)
+*
+*     Converts an arbitrary ascii buffer into its base_58 equivalent. This is largely used for converting hex digests, or
+*     other such things which cannot conveniently be converted to an integral.
+*
+*     :param input:     An input buffer
+*     :param length:    The length of said buffer
+*     :param res_len:   A pointer to the return buffer's length
+*     :param minDigits: The minimum number of base_58 digits you would like to get back
+*
+*     :returns: A buffer containing the base_58 equivalent of the provided buffer.
+*/
+
 static char *ascii_to_base_58(const char *input, size_t length, size_t *res_len, size_t minDigits) {
     char *result = ascii_to_base_58_(input, length, res_len);
     if (length < minDigits) {
@@ -155,6 +266,26 @@ static char *ascii_to_base_58(const char *input, size_t length, size_t *res_len,
 #ifdef __cplusplus
 }
 
+/**
+* C++ Only Section
+* ~~~~~~~~~~~~~~~~
+*
+* .. cpp:function:: static std::string ascii_to_base_58(std::string input)
+*
+*     Converts an arbitrary binary buffer into its base_58 equivalent.
+*
+*     This is a shortcut version of :c:func:`ascii_to_base_58`, with arguments:
+*
+*     - ``input.c_str()``
+*     - ``input.length()``
+*     - ``&size_placeholder``
+*     - ``1``
+*
+*     :param input: A :cpp:type:`std::string` which contains the buffer you wish to convert
+*
+*     :returns: A :cpp:type:`std::string` which contains the resulting data
+*/
+
 static string ascii_to_base_58(string input)   {
     size_t res_size = 0;
     char *c_string = ascii_to_base_58(input.c_str(), input.length(), &res_size, 1);
@@ -162,6 +293,17 @@ static string ascii_to_base_58(string input)   {
     free(c_string);
     return result;
 }
+
+/**
+* .. cpp:function:: static std::string to_base_58(unsigned long long i)
+*
+*     Converts an integral value into its base_58 equivalent. This takes the data from
+*     :c:func:`to_base_58` and converts it to a :cpp:type:`std::string`.
+*
+*     :param i: The integral value you wish to convert
+*
+*     :returns: A :cpp:type:`std::string` which contains the resulting data
+*/
 
 static inline string to_base_58(unsigned long long i)   {
     size_t len = 0;
