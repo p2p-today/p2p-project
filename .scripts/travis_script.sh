e47ecbec434830dc98e036ec5c6da2b9fab9cbed
Gabe Appleton <gappleto97+development@gmail.com>
2016-10-09 18:18:56 -0400

    Add missing file

---
+++ .scripts/travis_script.sh
@@ -0,0 +1,12 @@
+if [ $py_ver ]; then
+    pip install pytest-coverage codecov
+    py.test -vv --cov=./py_src/ ./py_src/
+    python setup.py sdist --universal && pip install --no-index --find-links=./dist/ py2p
+    mv .coverage .covvv
+    make cpytest cov=true
+    mv .covvv .coverage.1
+    mv .coverage .coverage.2
+else
+    make jstest;
+    make ES5test;
+fi
