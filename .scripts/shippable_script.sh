cd17ed801200f19a11d0e7f701b406af1f4170b2
Gabe Appleton <gappleto97+development@gmail.com>
2016-10-10 15:25:01 -0400

    Update shippable config

--- .scripts/shippable_script.sh
+++ .scripts/shippable_script.sh
@@ -11,7 +11,7 @@ if [ $pyver ]; then
     coverage xml
     codecov --token=d89f9bd9-27a3-4560-8dbb-39ee3ba020a5 --file=coverage.xml
 elif [ $jsver ]; then
-    sudo apt install build-essential libssl-dev
+    sudo apt-get install build-essential libssl-dev
     wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
