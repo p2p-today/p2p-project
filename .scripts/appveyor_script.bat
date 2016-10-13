ab019acc550091612e3c38329c32d46d86f2ff0a
Gabe Appleton <gappleto97+development@gmail.com>
2016-10-13 16:31:43 -0400

    Add error checking

--- .scripts/appveyor_script.bat
+++ .scripts/appveyor_script.bat
@@ -4,13 +4,13 @@ IF DEFINED PIP (
     %PYPY%
     %PIP% install pytest-coverage codecov cryptography
     cd %HOME%
-    %RUN% -m pytest -c setup.cfg --cov=./py_src/ ./py_src/
+    %RUN% -m pytest -c setup.cfg --cov=./py_src/ ./py_src/ || goto :error
     %RUN% setup.py sdist --universal
     %PIP% install --no-index --find-links=.\\dist\\ py2p
     %RUN% setup.py build
     FOR /F %%v IN ('%RUN% -c "import sys, sysconfig; print(\'{}.{}-{v[0]}.{v[1]}\'.format(\'lib\', sysconfig.get_platform(), v=sys.version_info))"') DO SET BUILD_DIR=%%v
     ren .coverage .covvv
-    %RUN% -m pytest -c setup.cfg --cov=build\\%BUILD_DIR% build\\%BUILD_DIR%
+    %RUN% -m pytest -c setup.cfg --cov=build\\%BUILD_DIR% build\\%BUILD_DIR% || goto :error
     ren .covvv .coverage.1
     ren .coverage .coverage.2
     %COV% combine
@@ -21,7 +21,11 @@ IF DEFINED PIP (
     powershell -Command "Install-Product node $env:NODE"
     npm install .
     npm install -g mocha babel-cli
-    mocha js_src\\test\\*
+    mocha js_src\\test\\* || goto :error
     babel js_src --out-dir build\\es5
-    mocha build\\es5\\test\\*
+    mocha build\\es5\\test\\* || goto :error
 )
+
+:error
+ECHO Failed with error #%errorlevel%.
+exit /b %errorlevel%
