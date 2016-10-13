7eb3e977f835ba1e8604ad24b1b9f1a3fa1f4a72
Gabe Appleton <gappleto97+development@gmail.com>
2016-10-13 16:49:58 -0400

    Appveyor debugging

--- .scripts/appveyor_script.bat
+++ .scripts/appveyor_script.bat
@@ -8,7 +8,7 @@ IF DEFINED PIP (
     %RUN% setup.py sdist --universal
     %PIP% install --no-index --find-links=.\\dist\\ py2p
     %RUN% setup.py build
-    FOR /F %%v IN ('%RUN% -c "import sys, sysconfig; print(\'{}.{}-{v[0]}.{v[1]}\'.format(\'lib\', sysconfig.get_platform(), v=sys.version_info))"') DO SET BUILD_DIR=%%v
+    FOR /F %%v IN (%RUN% -c "import sys, sysconfig; print('{}.{}-{v[0]}.{v[1]}'.format('lib', sysconfig.get_platform(), v=sys.version_info))") DO SET BUILD_DIR=%%v
     ren .coverage .covvv
     %RUN% -m pytest -c setup.cfg --cov=build\\%BUILD_DIR% build\\%BUILD_DIR% || goto :error
     ren .covvv .coverage.1
@@ -16,6 +16,7 @@ IF DEFINED PIP (
     %COV% combine
     %COV% xml
     %RUN% -c "import codecov; codecov.main('--token=d89f9bd9-27a3-4560-8dbb-39ee3ba020a5', '--file=coverage.xml')"
+    goto :EOF
 ) ELSE (
     dir C:\avvm\node
     powershell -Command "Install-Product node $env:NODE"
@@ -24,6 +25,7 @@ IF DEFINED PIP (
     mocha js_src\\test\\* || goto :error
     babel js_src --out-dir build\\es5
     mocha build\\es5\\test\\* || goto :error
+    goto :EOF
 )
 
 :error
