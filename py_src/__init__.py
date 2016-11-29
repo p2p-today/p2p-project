08945e8ab5e6ca1fa560f8055db7bc447292215f
the-zebulan <the-zebulan@users.noreply.github.com>
2016-11-28 23:36:28 -0700

    PEP8 fixes

--- py_src/__init__.py
+++ py_src/__init__.py
@@ -4,31 +4,31 @@ In this module:
 
     Constants
 
-      * __version__: A string containing the major, minor, and patch release
-                        number.
-      * version_info: A tuple version of the above
-      * protocol_version: A string containing the major and minor release number.
-                            This refers to the underlying protocol
+      * __version__:         A string containing the major, minor, and patch
+                                 release number.
+      * version_info:        A tuple version of the above
+      * protocol_version:    A string containing the major and minor release
+                                 number. This refers to the underlying protocol
       * node_policy_version: A string containing the build number associated
-                                with this version. This refers to the node
-                                and its policies.
+                                 with this version. This refers to the node
+                                 and its policies.
 
     Classes
 
       * mesh_socket(addr, port, out_addr=None, debug_level=0,
                     prot=py2p.mesh.default_protocol):
-          -  addr: The address you'd like to bind to
-          -  port: The port you'd like to bind to
-          -  out_addr: Your outward-facing address, if that is different
-                        from (addr, port)
-          -  prot: The py2p.base.protocol object you'd like to use
+          -  addr:        The address you'd like to bind to
+          -  port:        The port you'd like to bind to
+          -  out_addr:    Your outward-facing address, if that is different
+                              from (addr, port)
+          -  prot:        The py2p.base.protocol object you'd like to use
           -  debug_level: The verbosity at which this and its associated
-             py2p.mesh.mesh_daemon prints debug information
+                              py2p.mesh.mesh_daemon prints debug information
 
 Submodules:
 
     * base:        A library of common functions and classes to enable mesh
-                    and the planned chord
+                       and the planned chord
     * mesh:        A library to deal with mesh networking
     * chord:       A planned library to deal with distributed hash tables
     * ssl_wrapper: A shortcut library to generate peer-to-peer ssl.SSLSockets
--- py_src/ssl_wrapper.py
+++ py_src/ssl_wrapper.py
@@ -13,7 +13,8 @@ from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives.serialization import Encoding, PrivateFormat, NoEncryption
+from cryptography.hazmat.primitives.serialization import (
+    Encoding, PrivateFormat, NoEncryption)
 from cryptography.x509.oid import NameOID
 
 
