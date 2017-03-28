c3128aa04f60168b3fb84e57df9396c6c5015a8d
Gabe Appleton <gabe@gabeappleton.me>
2017-03-27 20:44:06 -0400

    Fix parse error

--- js_src/js2p.js
+++ js_src/js2p.js
@@ -42,14 +42,19 @@ m.version = m.base.version;
 m.version_info = m.base.version_info;
 
 function _get_database()    {
-    return {'SSL': {
-                'euclid': ['euclid.nmu.edu', 44566],
-                'turing': ['turing.nmu.edu', 44566],
-                'p2p.today': ['blog.p2p.today', 44566]},
+    return {
             'Plaintext': {
                 'euclid': ['euclid.nmu.edu', 44565],
                 'turing': ['turing.nmu.edu', 44565],
-                'p2p.today': ['blog.p2p.today', 44565]
+                'p2p.today': ['blog.p2p.today', 44565],
+            'SSL': {
+                'euclid': ['euclid.nmu.edu', 44566],
+                'turing': ['turing.nmu.edu', 44566],
+                'p2p.today': ['blog.p2p.today', 44566]},
+            'ws': {
+                'euclid': ['euclid.nmu.edu', 44567],
+                'turing': ['turing.nmu.edu', 44567],
+                'p2p.today': ['blog.p2p.today', 44567]}
             }};
 }
 
@@ -76,6 +81,7 @@ m.bootstrap = function bootstrap(socket_type, protocol, addr, port, ...args)    
 
     seed.once('connect', function on_connect(_) {
         let request = seed.get(protocol.id);
+        let id_ = ret.id;
         request.then(function on_receipt(dct)   {
             for (let key of new Set(Object.keys(dct)))  {
                 if (ret.routing_table.size > 4) {
@@ -89,10 +95,9 @@ m.bootstrap = function bootstrap(socket_type, protocol, addr, port, ...args)    
                     catch(e)    {}
                 }
             }
-            let id_ = ret.id;
             seed.apply_delta(proto.id, {id_: ret.out_addr}).catch(console.warn);
         }).catch((err)=>{
-            seed.apply_delta(proto.id, {ret.id: ret.out_addr}).catch(console.warn);
+            seed.apply_delta(proto.id, {id_: ret.out_addr}).catch(console.warn);
         });
 
         for (let id_ of seed.routing_table.keys())  {
