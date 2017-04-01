9f60ce02903a8294786195ee25b65571e081a60a
Gabe Appleton <gabe@gabeappleton.me>
2017-04-01 16:29:36 -0400

    Add seed command (needs close method, command keep-alive)

--- js_src/cli.js
+++ js_src/cli.js
@@ -9,17 +9,33 @@ else {
     m = root;
 }
 
-const js2p = require('./js2p.js');
 const equal = require('equal');
 
+m.seed_nodes = {}
+
 m.cli_seed = function(addr, port, transport, out_addr, verbosity) {
-    console.log(addr, port, transport, out_addr, verbosity);
+    const js2p = require('./js2p.js');
+    m.seed_nodes[transport] = js2p.bootstrap(
+        js2p.chord.ChordSocket,
+        new js2p.base.Protocol('bootstrap', transport),
+        addr,
+        port,
+        out_addr,
+        verbosity
+    );
+    m.seed_nodes[transport].join();
 };
 
 const argv = require('yargs')
     .usage('Usage: js2p <command> [options]')
     .option('v', {describe: 'the verbosity level you would like'})
     .count('v')
+    .command('repl',
+             'Opens an interactive interface',
+             ()=>{},
+             (yargv)=>{
+                require('./ctl.js');
+             })
     .command('seed <addr> <port> [transport] [outward_address] [outward_port]',
              'Seed the bootstrap network for a given transport method',
              ()=>{},
@@ -27,21 +43,16 @@ const argv = require('yargs')
                 let transport = yargv.transport || 'Plaintext';
                 let out_addr = undefined;
                 if (yargv.outward_port !== undefined)   {
-                    out_addr = [outward_address, outward_port];
+                    out_addr = [yargv.outward_address, parseInt(yargv.outward_port)];
                 }
                 else if (yargv.outward_address !== undefined)   {
-                    out_addr = [outward_address, port];
+                    out_addr = [yargv.outward_address, parseInt(yargv.port)];
                 }
-                m.cli_seed(yargv.addr, yargv.port, transport, out_addr, yargv.v);
+                m.cli_seed(yargv.addr, parseInt(yargv.port), transport, out_addr, yargv.v);
     })
     .example('js2p seed 0.0.0.0 44566 SSL -vvvvv')
     .example('js2p seed 0.0.0.0 44565 Plaintext euclid.nmu.edu')
-    .command('repl',
-             'Opens an interactive interface',
-             ()=>{},
-             (yargv)=>{
-                require('./ctl.js');
-             })
     .help('h')
     .alias('h', 'help')
+    .demandCommand(1, 'Please enter a single command')
     .argv;
--- js_src/ctl.js
+++ js_src/ctl.js
@@ -22,5 +22,34 @@ vorpal
     });
 
 vorpal
+    .command('list seeds', 'List the nodes that you currently have seeding the network')
+    .action(function(args, repl)    {
+        Object.keys(cli.seed_nodes).forEach((transport)=>{
+            let node = cli.seed_nodes[transport];
+            let formatted_transport = `${transport}:${' '.repeat(10 - transport.length)}`;
+            console.log(`${formatted_transport}${node.out_addr[0]}:${node.out_addr[1]} (${node.id})`);
+            console.log(`\t\tseeding ${node.routing_table.size} nodes`);
+            console.log(`\t\tseeding ${node.__keys.size} networks`);
+        });
+        repl();
+    });
+
+vorpal
+    .command('stop seeding [transport]', 'Halts seeding a specific transport method if given, all if not')
+    .action(function(args, repl)    {
+        if (args.transport) {
+            // cli.seed_nodes[args.transport].close();
+            delete cli.seed_nodes[args.transport];
+        }
+        else    {
+            Object.keys(cli.seed_nodes).forEach((transport)=>{
+                // cli.seed_nodes[transport].close();
+                delete cli.seed_nodes[transport];
+            });
+        }
+        repl();
+    })
+
+vorpal
   .delimiter('[js2p]#')
   .show();
